bda9ef9ed235e8b14edd1aeaf56dcd509dc4afb6
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2025-08-22 23:31:25 +0545

    feat: adjust margin and spacing in skills section for improved layout

--- components/About.jsx
+++ components/About.jsx
@@ -168,7 +168,7 @@ const About = () => {
                   </div> 
                 </TabsContent> 
                 <TabsContent value="skills"> 
-                  <div className="text-center xl:text-left mb-20"> 
+                  <div className="text-center xl:text-left "> 
                     <h3 className="h3 mb-8">What I Use</h3> 
                     {skillData.map((section, idx) => ( 
                       <div key={idx} className="mb-8"> 
@@ -176,7 +176,7 @@ const About = () => {
                         <div className="border-b border-border mb-6"></div> 
                         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6"> 
                           {section.data.map((item, index) => ( 
-                            <div key={index} className="flex flex-col items-center gap-2"> 
+                            <div key={index} className="flex flex-col items-center gap-2 mb-10"> 
                               <img src={item.imgPath} alt={item.name} className="w-12 h-12 object-contain" /> 
                               <span className="text-sm text-gray-300">{item.name}</span> 
                             </div> 
